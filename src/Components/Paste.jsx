d57846bcbc3bcd9a00e051bc5759c808565bc702
Hitesh Jindam <133262032+hit2733@users.noreply.github.com>
2023-10-09 12:31:27 +0530

    Create Paste.jsx

---
+++ src/Components/Paste.jsx
@@ -0,0 +1,13 @@
+function Paste() {
+  return (
+    <>
+      <input
+        placeholder="paste here"
+        className="h-9 w-72
+        ml-16 mt-4 rounded-md text-red-600 font-bold "
+      ></input>
+    </>
+  );
+}
+
+export default Paste;
